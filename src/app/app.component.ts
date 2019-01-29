1a4ac0f26627d65c90a1f10c896e1bc99d122453
andrea.iraggi <andrea.iraggi@accenture.com>
2019-01-29 19:03:46 +0100

    cose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,13 +26,20 @@ export class AppComponent {
   buildUrl = "http://ddragon.leagueoflegends.com/cdn/9.2.1/img/item/";
   summonerSpellUrl = "http://ddragon.leagueoflegends.com/cdn/9.2.1/img/spell/";
   build: String[] = [];
-  builds:any[]=[];
-  onGetInfo(name) {
-    this.service.getAccountInfo(name).subscribe((res: any) => {
+  builds: any[] = [];
+  championSkill: any[] = [];
+  championSelected: String;
+  onGetInfo(name, region = 'euw') {
+    this.service.getAccountInfo(name, region).subscribe((res: any) => {
       console.log(res);
       this.summonerID = res.id;
       this.accountID = res.accountId;
-      this.service.getMatchForAccount(this.summonerID).subscribe((res: any) => { this.gamePlayers = res.participants; console.log(res.participants) })
+      this.service.getMatchForAccount(this.summonerID, region).subscribe((res: any) => {
+        this.gamePlayers = res.participants;
+        console.log(res.participants);
+        this.gamePlayers.forEach(p => { this.service.getRankedPosition(p.summonerId, region).subscribe((res: any) => p.gameCustomizationObjects = res) });
+      })
+      console.log(this.gamePlayers);
     }, err => console.log(err));
   }
   getChampionsList() {
@@ -78,32 +85,70 @@ export class AppComponent {
     return summ;
   }
 
-  onBuildSearch(name: String) {
+  onBuildSearch(name: String, role: string) {
     this.builds = [];
-    let i=0
-    this.service.getAccountInfo('danto 0tohero').subscribe((res: any) => {
+    let i = 0;
+    let player: string;
+    let region: string;
+    if (role == 'Top' || role == '') {
+      player = 'tfblade';
+      region = 'na'
+    } else if (role == 'Jungle') {
+      player = 'Danto 0tohero';
+      region = 'euw'
+    } else if (role == 'Mid') {
+      player = 'Shiphtur';
+      region = 'na'
+    } else if (role == 'ADC') {
+      player = 'imaqtpie';
+      region = 'na'
+    } else if (role == 'Support') {
+      player = 'aphromoo';
+      region = 'na'
+    }
+    this.service.getAccountInfo(player, region).subscribe((res: any) => {
       let championId = this.onSearchChampionId(name);
-      this.service.getMatchHistory(res.accountId, championId).subscribe((res: any) => {
-        while (i < 5) {
-          this.service.getMatchInfo(res.matches[i].gameId).subscribe((res: any) => {
+      this.service.getMatchHistory(res.accountId, championId, region).subscribe((res: any) => {
+        console.log(res)
+        for (i=0;i<5;i++) {
+          this.service.getMatchInfo(res.matches[i].gameId, region).subscribe((res: any) => {
             res.participants.forEach(p => {
               if (p.championId == championId) {
-                let b:String[]=[];
-                b.push(this.buildUrl + p.stats.item0 + ".png");
-                b.push(this.buildUrl + p.stats.item1 + ".png");
-                b.push(this.buildUrl + p.stats.item2 + ".png");
-                b.push(this.buildUrl + p.stats.item4 + ".png");
-                b.push(this.buildUrl + p.stats.item5 + ".png");
-                b.push(this.buildUrl + p.stats.item6 + ".png");
+                let b: String[] = [];
+                let d: Date = new Date(res.gameCreation);
+                b.push(p.stats.item0 == 0 ? '/assets/EmptyIcon.png' : this.buildUrl + p.stats.item0 + ".png");
+                b.push(p.stats.item1 == 0 ? '/assets/EmptyIcon.png' : this.buildUrl + p.stats.item1 + ".png");
+                b.push(p.stats.item2 == 0 ? '/assets/EmptyIcon.png' : this.buildUrl + p.stats.item2 + ".png");
+                b.push(p.stats.item4 == 0 ? '/assets/EmptyIcon.png' : this.buildUrl + p.stats.item4 + ".png");
+                b.push(p.stats.item5 == 0 ? '/assets/EmptyIcon.png' : this.buildUrl + p.stats.item5 + ".png");
+                b.push(p.stats.item6 == 0 ? '/assets/EmptyIcon.png' : this.buildUrl + p.stats.item6 + ".png");
                 b.push(this.summonerSpellUrl + this.onSearchSummonerName(p.spell1Id) + ".png");
                 b.push(this.summonerSpellUrl + this.onSearchSummonerName(p.spell2Id) + ".png");
+                b.push(d.getDate() + "/" + (d.getUTCMonth() + 1) + "/" + d.getFullYear());
                 this.builds.push(b);
               }
             });
           });
-          i=i+1;
         }
       })
     })
+  }
+  onGetChampionSkill(championName) {
+    if (this.championSelected == championName) {
+      this.championSelected = '';
+    } else {
+      this.service.getChampionSkill(championName).subscribe((res: any) => {
+        this.championSelected = championName;
+        console.log(res.data)
+        this.championSkill = [];
+        res.data[championName].spells.forEach(spell => {
+          let a = { 'cd': '', 'mana': '', 'image': 'http://ddragon.leagueoflegends.com/cdn/9.2.1/img/spell/' };
+          a.cd = spell.cooldownBurn;
+          a.mana = spell.costBurn;
+          a.image += spell.image.full;
+          this.championSkill.push(a);
+        });
+      })
+    }
   }
 }
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
-let key="?api_key=RGAPI-7775dea9-d309-44bd-b8a9-7185d67d1f70"
-let domain="https://euw1.api.riotgames.com/lol"
-
+let key="?api_key=RGAPI-a9083581-e521-47e5-950c-cc222e7be181"
+let domain="1.api.riotgames.com/lol"
+let protocol='https://';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,11 +12,11 @@ export class RestService {
 
   constructor(private http:HttpClient) { }
 
-  getAccountInfo(name : string){
-    return this.http.get(domain+"/summoner/v4/summoners/by-name/"+name+key);
+  getAccountInfo(name : string, region){
+    return this.http.get(protocol+region+domain+"/summoner/v4/summoners/by-name/"+name+key);
   }
-  getMatchForAccount(id){
-    return this.http.get(domain+"/spectator/v4/active-games/by-summoner/"+id+key);
+  getMatchForAccount(id,region){
+    return this.http.get(protocol+region+domain+"/spectator/v4/active-games/by-summoner/"+id+key);
   }
   getChampions(){
     return this.http.get('./assets/champion.json');
@@ -27,10 +27,16 @@ export class RestService {
   getSpells(){
     return this.http.get('./assets/summonerSpell.json');
   }
-  getMatchHistory(accountId,championName){
-    return this.http.get(domain+'/match/v4/matchlists/by-account/'+accountId+key+'&champion='+championName);
+  getMatchHistory(accountId,championName,region){
+    return this.http.get(protocol+region+domain+'/match/v4/matchlists/by-account/'+accountId+key+'&champion='+championName);
   }
-  getMatchInfo(matchId){
-    return this.http.get(domain+"/match/v4/matches/"+matchId+key);
+  getMatchInfo(matchId,region){
+    return this.http.get(protocol+region+domain+"/match/v4/matches/"+matchId+key);
+  }
+  getRankedPosition(summonerId,region){
+    return this.http.get(protocol+region+domain+"/league/v4/positions/by-summoner/"+summonerId+key);
+  }
+  getChampionSkill(championName){
+    return this.http.get("http://ddragon.leagueoflegends.com/cdn/9.2.1/data/en_US/champion/"+championName+".json")
   }
 }
